c08884523e6680307950ea070d80bbcee456cd10
irsyadfauzan <irsyadfauzan018@gmail.com>
2024-06-19 03:08:00 +0700

    add all pulls modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MainLogicService } from './services/main-logic.service';
+import { PullItem } from 'src/interface/pull-item-model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wuwa-gacha-simulator';
+  pullArray : PullItem[] = []
+  allArray : PullItem[] = []
+  showModal : Boolean = false
+  showModalAllPulls: Boolean = false
+  count : number = 0
+  astriteCount : number = 0
+
+  constructor(private mainLogicService : MainLogicService) {}
+  
+  convene(amount : number) {
+    this.mainLogicService.convene(amount)
+    this.pullArray = this.mainLogicService.pullArray
+    this.count = this.mainLogicService.count
+    this.allArray = this.mainLogicService.allArray
+    this.astriteCount = this.mainLogicService.astriteSpent
+    this.openModal()
+  }
+
+  closeModal() {
+    this.showModal = false
+  }
+
+  openModal() {
+    this.showModal = true
+  }
+
+  closeModalAllPulls() {
+    this.showModalAllPulls = false
+  }
+
+  openModalAllPulls() {
+    this.showModalAllPulls = true
+  }
 }
---
+++ src/interface/pull-item-model.ts
@@ -0,0 +1,4 @@
+export interface PullItem {
+    name : string;
+    type : string;
+}
